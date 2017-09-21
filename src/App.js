15bd12184125fc1668ee57365ddf2316687d7b0a
Colombianito66 <colombianito66@gmail.com>
2017-09-21 16:46:47 -0700

    create AllPosts container

--- src/App.js
+++ src/App.js
@@ -6,14 +6,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Stevie Is Magic InsideApp.js</h2>
         </div>
+
         <p className="App-intro">
           This file is exported to index.js for ultimate page render as DOM 'root'
           All page routes from react-router-dom will go in this component
         </p>
+
+        <AllPosts /> 
+
       </div>
     );
   }
