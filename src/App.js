a8e672ab4e5e8660676b521e3ae6df89f13acfe1
Colombianito66 <colombianito66@gmail.com>
2017-09-21 17:39:10 -0700

    set up component routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import AllPosts from './components/AllPosts.js';
-
+import Navigation from './components/Navigation.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Stevie Is Magic InsideApp.js</h2>
-        </div>
-
-        <p className="App-intro">
-          This file is exported to index.js for ultimate page render as DOM 'root'
-          All page routes from react-router-dom will go in this component
-        </p>
-        <Navigation />
-        <AllPosts /> 
-
+        <Route path='/' component={ Navigation } />
+        <Route path='/posts' component={ AllPosts } />
+        <Route path='/posts/:post_id' component={ IndividualPost } />
       </div>
     );
   }
