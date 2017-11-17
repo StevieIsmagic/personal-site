2d73e66da274e06fb94688f5b27374684c12dc6e
Colombianito66 <colombianito66@gmail.com>
2017-11-17 03:19:14 -0500

    ad idem

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,23 @@ class Home extends React.Component {
   render () {
     return (
       <div>
-        <h1> INSIDE HOME COMPONENT </h1>
+        <h1>Mon Nom Est Stevie Et Je T'aime</h1>
+          <p>
+            We are lovers of the beautiful,
+          </p>
+          <p>
+            Yet simple in our tastes.
+          </p>
+          <p>
+            And we cultivate the arts..
+          </p>
+          <p>
+            Without loss of manliness.
+          </p>
       </div>
     )
   }
 }
 
 export default Home;
+
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,8 +3,8 @@ import React from 'react';
 var ulStyle = {
   background: "#eee",
   padding: "30px",
-  border: '10px solid black',
-  borderRadius: '5%',
+  border: '2px solid black',
+  borderRadius: '10%',
   margin: "20px",
   listStyle: "none",
   
@@ -16,24 +16,14 @@ class Portfolio extends React.Component{
 			<div>
 				<div className="flex layout-column layout-align-space-between" style={ulStyle}>
 					<div className="flex layout-row layout-align-space-between">
-						<div className="blue-box" style={ulStyle}>1</div>
-						<div className="blue-box" style={ulStyle}>2</div>
-						<div className="blue-box" style={ulStyle}>3</div>
+						<div className="blue-box" style={ulStyle}>Project 1</div>
+						<div className="blue-box" style={ulStyle}>Project 2</div>
+						<div className="blue-box" style={ulStyle}>Project 3</div>
 					</div>
 					<div className="flex layout-row layout-align-space-around-center">
-						<div className="blue-box" style={ulStyle}>1</div>
-						<div className="blue-box" style={ulStyle}>2</div>
-						<div className="blue-box" style={ulStyle}>3</div>
-					</div>
-					<div className="flex layout-row layout-align-start-center">
-						<div className="blue-box" style={ulStyle}>1</div>
-						<div className="blue-box" style={ulStyle}>2</div>
-						<div className="blue-box" style={ulStyle}>3</div>
-					</div>
-					<div className="flex layout-row layout-align-end-center">
-						<div className="blue-box" style={ulStyle}>1</div>
-						<div className="blue-box" style={ulStyle}>2</div>
-						<div className="blue-box" style={ulStyle}>3</div>
+						<div className="blue-box" style={ulStyle}>Project 4</div>
+						<div className="blue-box" style={ulStyle}>Project 5</div>
+						<div className="blue-box" style={ulStyle}>Project 6</div>
 					</div>
 				</div>
 			</div>
